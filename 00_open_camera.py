29dac6e702d601fb63c225cd601d2ce6448da89d
wangga03 <aswanggasadu@gmail.com>
2024-05-19 00:03:16 +0700

    dff

--- 00_open_camera.py
+++ 00_open_camera.py
@@ -4,7 +4,8 @@ def main(capture) :
 
     while True :
         ret, frame = capture.read()
-        cv.imshow("frame", frame)
+        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        cv.imshow("frame", gray)
         if cv.waitKey(1) & 0xFF == ord('q') :
             break
 
@@ -12,4 +13,5 @@ def main(capture) :
     cv.destroyAllWindows()
 if __name__ == "__main__" :
     cap = cv.VideoCapture(0)
+
     main(cap)
